512b81c4ffacd0dfa47e6520880ae1b0698ae558
DATTvd <44138794+DATTvd@users.noreply.github.com>
2021-08-07 09:18:40 +0700

    dell

--- script.js
+++ script.js
@@ -1,34 +1,115 @@
-const itemSlide = document.querySelector('.carousel-inner');
-const slides = Array.from(itemSlide.children);
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
 const prevButton = document.querySelector('.carousel-control-prev');
 const nextButton = document.querySelector('.carousel-control-next');
-const dot = document.querySelector('.dot');
-const dots = Array.from(dot.children);
-
+const dotsNav = document.querySelector('.carousel-nav');
+const dots = Array.from(dotsNav.children);
+console.log(slides)
 const slideWidth = slides[0].getBoundingClientRect().width;
 
-// console.log(slideWidth);
 
-// arrange the slides next to one another
-// slides[0].style.left = slideWidth * 0 + 'px';
-// slides[1].style.left = slideWidth * 1 + 'px';
-// slides[2].style.left = slideWidth * 2 + 'px';
-// slides[3].style.left = slideWidth * 3 + 'px';
-// slides[4].style.left = slideWidth * 4 + 'px';
-// slides[5].style.left = slideWidth * 5 + 'px';
-
+//Set width for slide
 const setSlidePotision = (slide, index) => {
     slide.style.left = slideWidth * index + 'px';
 };
-
 slides.forEach(setSlidePotision);
+
+
+//move to slide
+const movetoSlide = (track, currentSlide, targetSlide) =>{
+    const currrentSlide = track.querySelector('.current-slide');
+    track.style.transform ='translateX(-' + targetSlide.style.left + ')';
+    currrentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+// when click left, back slide to the left
+
+prevButton.addEventListener('click', e => {
+    const currrentSlide = track.querySelector('.current-slide');
+    const prevSlide = currrentSlide.previousElementSibling; // lấy element ở trước(trên)
+   movetoSlide(track, currrentSlide, prevSlide);
+})
 
 // when click right, move slide to the right
 
 nextButton.addEventListener('click', e => {
-    const currrentSlide = itemSlide.querySelector('.current-slide');
-    const nextSlide = currrentSlide.nextElementSibling;
-    const amountToMove = nextSlide.style.left;
+    const currrentSlide = track.querySelector('.current-slide');
+    const nextSlide = currrentSlide.nextElementSibling; // lấy element ở sau (dưới)
    // move to the next silde 
-    itemSlide.style.transform = 'translateX(' + amountToMove + ')';
+   movetoSlide(track, currrentSlide, nextSlide);
 })
+
+
+// Init arrWidth
+var arrWidth = [];
+
+slideWidthArr = slideWidth * 0 + 'px';
+arrWidth.push(slideWidthArr)
+slideWidthArr = slideWidth * 1 + 'px';
+arrWidth.push(slideWidthArr)
+slideWidthArr = slideWidth * 2 + 'px';
+arrWidth.push(slideWidthArr)
+slideWidthArr = slideWidth * 3 + 'px';
+arrWidth.push(slideWidthArr)
+slideWidthArr = slideWidth * 4 + 'px';
+arrWidth.push(slideWidthArr)
+slideWidthArr = slideWidth * 5 + 'px';
+arrWidth.push(slideWidthArr)
+
+
+dotsNav.addEventListener("click", e => {
+    if (e.target.nodeName === "BUTTON") {
+    dots.forEach(item => item.classList.remove("active"));
+    
+    if (e.target.classList.contains("first")) {
+        track.style.transform = 'translateX(-' + arrWidth[0] + ')';      
+        e.target.classList.add("active");
+
+    } else if (e.target.classList.contains("second")) {
+        track.style.transform = 'translateX(-' + arrWidth[1] + ')';
+        e.target.classList.add("active");
+    } else if (e.target.classList.contains('third')){
+        track.style.transform = 'translateX(-' + arrWidth[2] + ')';
+        e.target.classList.add('active');
+    }
+    else if (e.target.classList.contains('fourth')){
+      track.style.transform = 'translateX(-' + arrWidth[3] + ')';
+      e.target.classList.add('active');
+    }
+    else if (e.target.classList.contains('fifth')){
+      track.style.transform = 'translateX(-' + arrWidth[4] + ')';
+      e.target.classList.add('active');
+    }
+    else if (e.target.classList.contains('sixth')){
+      track.style.transform = 'translateX(-' + arrWidth[5] + ')';
+      e.target.classList.add('active');
+  }
+  }
+});
+
+// prevButton.addEventListener("click", e => {
+
+//     slides[0].getAttribute('id') == ('first')
+//         track.style.transform = 'translateX(-' + arrWidth[0] + ')';      
+
+
+//     slides[1].getAttribute('id') == ('second')
+//         track.style.transform = 'translateX(-' + arrWidth[1] + ')';
+        
+//     slides[2].getAttribute('id') == ('third')
+//         track.style.transform = 'translateX(-' + arrWidth[2] + ')';
+        
+//     slides[3].getAttribute('id') == ('fourth')
+//       track.style.transform = 'translateX(-' + arrWidth[3] + ')';
+    
+//     slides[4].getAttribute('id') == ('fifth')
+//       track.style.transform = 'translateX(-' + arrWidth[4] + ')';
+//     slides[5].getAttribute('id') == ('sixth')
+//       track.style.transform = 'translateX(-' + arrWidth[5] + ')';    
+
+//   });
+
+
+
+
