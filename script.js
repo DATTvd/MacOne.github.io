c1a2e03926ef0172267175688aabab70642d29e5
DATTvd <44138794+DATTvd@users.noreply.github.com>
2021-08-07 11:45:03 +0700

    dell

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const prevButton = document.querySelector('.carousel-control-prev');
 const nextButton = document.querySelector('.carousel-control-next');
 const dotsNav = document.querySelector('.carousel-nav');
 const dots = Array.from(dotsNav.children);
-console.log(slides)
 const slideWidth = slides[0].getBoundingClientRect().width;
 
 
@@ -21,41 +20,54 @@ const movetoSlide = (track, currentSlide, targetSlide) =>{
     track.style.transform ='translateX(-' + targetSlide.style.left + ')';
     currrentSlide.classList.remove('current-slide');
     targetSlide.classList.add('current-slide');
+    
+}
+
+  const moveToDotPrev = (index) =>{
+  const indexTarget = index - 1; 
+  const currentDot = dotsNav.querySelector('.active');
+  currentDot.classList.remove('active');
+  const targetDot = dots[indexTarget];
+  targetDot.classList.add('active');
+}
+
+const moveToDotNext = (index) =>{
+  const indexTarget = parseInt(index) + 1; 
+  const currentDot = dotsNav.querySelector('.active');
+  currentDot.classList.remove('active');
+  const targetDot = dots[indexTarget];
+  targetDot.classList.add('active');
 }
 
 // when click left, back slide to the left
 
-prevButton.addEventListener('click', e => {
-    const currrentSlide = track.querySelector('.current-slide');
+auToSlide = prevButton.addEventListener('click', e => {
+    const currrentSlide = track.querySelector('.current-slide'); 
+    const index = currrentSlide.getAttribute('ID')
     const prevSlide = currrentSlide.previousElementSibling; // lấy element ở trước(trên)
-   movetoSlide(track, currrentSlide, prevSlide);
+    movetoSlide(track, currrentSlide, prevSlide);
+    moveToDotPrev(index);
 })
 
 // when click right, move slide to the right
 
 nextButton.addEventListener('click', e => {
     const currrentSlide = track.querySelector('.current-slide');
-    const nextSlide = currrentSlide.nextElementSibling; // lấy element ở sau (dưới)
+    const index = currrentSlide.getAttribute('ID')
+    const nextSlide = currrentSlide.nextElementSibling;
+    // lấy element ở sau (dưới)
    // move to the next silde 
    movetoSlide(track, currrentSlide, nextSlide);
+   moveToDotNext(index);
 })
 
 
 // Init arrWidth
 var arrWidth = [];
-
-slideWidthArr = slideWidth * 0 + 'px';
-arrWidth.push(slideWidthArr)
-slideWidthArr = slideWidth * 1 + 'px';
-arrWidth.push(slideWidthArr)
-slideWidthArr = slideWidth * 2 + 'px';
-arrWidth.push(slideWidthArr)
-slideWidthArr = slideWidth * 3 + 'px';
-arrWidth.push(slideWidthArr)
-slideWidthArr = slideWidth * 4 + 'px';
-arrWidth.push(slideWidthArr)
-slideWidthArr = slideWidth * 5 + 'px';
-arrWidth.push(slideWidthArr)
+for(i=0; i<slides.length; i++){
+  const slideWidthArr = slideWidth * i + 'px';
+  arrWidth.push(slideWidthArr)
+}
 
 
 dotsNav.addEventListener("click", e => {
@@ -69,46 +81,53 @@ dotsNav.addEventListener("click", e => {
     } else if (e.target.classList.contains("second")) {
         track.style.transform = 'translateX(-' + arrWidth[1] + ')';
         e.target.classList.add("active");
+        // prevSlideB = slides[0]
+        // targetSlideB = slides[1];
+        // prevSlideB.classList.remove('current-slide')
+        // targetSlideB.classList.add('.current-slide')
+        // console.log(targetSlideB);
+
     } else if (e.target.classList.contains('third')){
         track.style.transform = 'translateX(-' + arrWidth[2] + ')';
         e.target.classList.add('active');
+        // prevSlideB = slides[1]
+        // targetSlideB = slides[2];
+        // prevSlideB.classList.remove('current-slide')
+        // targetSlideB.classList.add('.current-slide')
+        // console.log(targetSlideB);
     }
     else if (e.target.classList.contains('fourth')){
-      track.style.transform = 'translateX(-' + arrWidth[3] + ')';
-      e.target.classList.add('active');
-    }
+        track.style.transform = 'translateX(-' + arrWidth[3] + ')';
+        e.target.classList.add('active');
+        // prevSlideB = slides[2]
+        // targetSlideB = slides[3];
+        // prevSlideB.classList.remove('current-slide')
+        // targetSlideB.classList.add('.current-slide')
+        // console.log(targetSlideB);
+    } 
     else if (e.target.classList.contains('fifth')){
-      track.style.transform = 'translateX(-' + arrWidth[4] + ')';
-      e.target.classList.add('active');
+        track.style.transform = 'translateX(-' + arrWidth[4] + ')';
+        e.target.classList.add('active');
+        // prevSlideB = slides[3]
+        // targetSlideB = slides[4];
+        // prevSlideB.classList.remove('current-slide')
+        // targetSlideB.classList.add('.current-slide')
+        // console.log(targetSlideB);
     }
     else if (e.target.classList.contains('sixth')){
-      track.style.transform = 'translateX(-' + arrWidth[5] + ')';
-      e.target.classList.add('active');
-  }
+        track.style.transform = 'translateX(-' + arrWidth[5] + ')';
+        e.target.classList.add('active');
+        // prevSlideB = slides[4]
+        // targetSlideB = slides[5];
+        // prevSlideB.classList.remove('current-slide')
+        // targetSlideB.classList.add('.current-slide')
+        // console.log(targetSlideB);
+    }
   }
 });
 
-// prevButton.addEventListener("click", e => {
-
-//     slides[0].getAttribute('id') == ('first')
-//         track.style.transform = 'translateX(-' + arrWidth[0] + ')';      
 
 
-//     slides[1].getAttribute('id') == ('second')
-//         track.style.transform = 'translateX(-' + arrWidth[1] + ')';
-        
-//     slides[2].getAttribute('id') == ('third')
-//         track.style.transform = 'translateX(-' + arrWidth[2] + ')';
-        
-//     slides[3].getAttribute('id') == ('fourth')
-//       track.style.transform = 'translateX(-' + arrWidth[3] + ')';
-    
-//     slides[4].getAttribute('id') == ('fifth')
-//       track.style.transform = 'translateX(-' + arrWidth[4] + ')';
-//     slides[5].getAttribute('id') == ('sixth')
-//       track.style.transform = 'translateX(-' + arrWidth[5] + ')';    
-
-//   });
 
 
 
